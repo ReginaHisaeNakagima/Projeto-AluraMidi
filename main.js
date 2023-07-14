86134fd3427f0b9a9061a2b420bf119da3f0563c
HisaeNakagima <1823289@aluno.univesp.br>
2023-07-14 09:29:09 -0300

    Utilizando a estrutura de repetição FOR

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-//enquanto
-while (contador < listaDeTeclas.length) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`; //template string
@@ -15,6 +13,4 @@ while (contador < listaDeTeclas.length) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-
-  contador = contador + 1;
 }
